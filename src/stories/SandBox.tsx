5d7a9a7069d75be75df086221e8806a3f29fcdf7
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-08-25 15:35:20 -0700

    upgade

--- src/stories/SandBox.tsx
+++ src/stories/SandBox.tsx
@@ -2,19 +2,6 @@ import React from 'react';
 
 import './button.css';
 
-export interface ButtonProps {
-  /** Is this the principal call to action on the page? */
-  primary?: boolean;
-  /** What background color to use */
-  backgroundColor?: string;
-  /** How large should the button be? */
-  size?: 'small' | 'medium' | 'large';
-  /** Button contents */
-  label: string;
-  /** Optional click handler */
-  onClick?: () => void;
-}
-
 /** Primary UI component for user interaction */
 export const SandBox = ({
   primary = false,
@@ -24,14 +11,31 @@ export const SandBox = ({
   ...props
 }: ButtonProps) => {
   const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+  React.useEffect(() => {
+    async function getData() {
+      const url =
+        "https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/636172";
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+  
+        const result = await response.body;
+        console.log("what is the result?!!", result);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+  
+    getData();
+  })
+
   return (
-    <button
-      type="button"
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
-      {...props}
-    >
-      {label}
-    </button>
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
   );
 };
