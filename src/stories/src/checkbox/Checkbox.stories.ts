3655a91050f7cbd4f4fcfa0171085757abb70814
Aaron Villanueva <aarone.villanueva@gmail.com>
2025-09-13 19:20:54 -0700

    complete select download table projet

--- src/stories/src/checkbox/Checkbox.stories.ts
+++ src/stories/src/checkbox/Checkbox.stories.ts
@@ -2,25 +2,21 @@ import type { Meta, StoryObj } from '@storybook/react-vite';
 
 import { fn } from 'storybook/test';
 
-import { Button } from './Button';
+import { Checkbox } from './Checkbox';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: 'Example/Button',
-  component: Button,
+  title: 'Example/Checkbox',
+  component: Checkbox,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'centered',
   },
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
-} satisfies Meta<typeof Button>;
+} satisfies Meta<typeof Checkbox>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
@@ -28,27 +24,27 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
-    label: 'Button',
+    hintId: "select-test-id",
+    hint: "clicking will select or deselect checkbox",
+    label: 'Select',
+    invisibleLabel: false,
   },
 };
 
-export const Secondary: Story = {
+export const InvisibleLabel: Story = {
   args: {
-    label: 'Button',
+    hintId: "select-test-id",
+    hint: "clicking will select or deselect checkbox",
+    label: 'visually hidden select label',
+    invisibleLabel: true,
   },
 };
 
-export const Large: Story = {
+export const Disabled: Story = {
   args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small: Story = {
-  args: {
-    size: 'small',
-    label: 'Button',
+    hintId: "select-test-id",
+    hint: "clicking will select or deselect checkbox",
+    label: 'Select',
+    disabled: true,
   },
 };
